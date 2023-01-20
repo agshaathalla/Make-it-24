2513357d43166fbe634b982adcf4c6ea0a4a682f
agshaathalla <agshaathalla@gmail.com>
2023-01-20 16:23:00 +0700

    add splash screen

---
+++ src/splashScreeen.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void splashScreen(){
+    std::cout <<"              _          _ _     ____  _  _   "<< std::endl;
+    std::cout <<"  /\\/\\   __ _| | _____  (_) |_  |___ \\| || |  "<< std::endl;
+    std::cout <<" /    \\ / _` | |/ / _ \\ | | __|   __) | || |_ "<< std::endl;
+    std::cout <<"/ /\\/\\ \\ (_| |   <  __/ | | |_   / __/|__   _|"<< std::endl;
+    std::cout <<"\\/    \\/\\__,_|_|\\_\\___| |_|\\__| |_____|  |_|  "<< std::endl;
+    std::cout << std::endl;
+    std::cout <<"       ------------------------------"<< std::endl;
+    std::cout <<"       Nama : Agsha Athalla Nurkareem"<< std::endl;
+    std::cout <<"               NIM : 13521027"<< std::endl;
+    std::cout <<"            Mata Kuliah : IF2211"<< std::endl;
+    std::cout <<"       ------------------------------"<< std::endl;
+    std::cout <<""<< std::endl;
+    std::cout <<""<< std::endl;
+}
+
